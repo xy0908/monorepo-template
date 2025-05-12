61aadc8ee5abd208627acc3f0a4099334baa50c5
小羊aaa <81454465+xy0908@users.noreply.github.com>
2025-05-12 10:20:31 +0800

    chore: 更新.gitignore并移除未使用的组件声明
    更新.gitignore文件以忽略auto-imports和components目录下的TypeScript文件，同时移除apps/frontend/components.d.ts中未使用的XyButton组件声明

--- apps/frontend/components.d.ts
+++ apps/frontend/components.d.ts
@@ -10,6 +10,5 @@ declare module 'vue' {
   export interface GlobalComponents {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    XyButton: typeof import('@demo/ui')['XyButton']
   }
 }
